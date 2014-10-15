3240ec85ee331645acf7fe76ff471d4dffa8523e
Matthew Zipkin <Matthew.Zipkin@gmail.com>
2014-10-15 15:09:23 -0700

    eee

--- edit.php
+++ edit.php
@@ -1,13 +1,13 @@
 <?
 	$PW = '59dcd687a2ce676d99e7624545efd5ed797dd2ec';
 	$sponsors = file_get_contents('sponsors.txt');
-	//$endorsements = file_get_contents('endorsements.txt');
+	$e = file_get_contents('e.txt');
 	
 	if (!empty($_POST['password'])){
 		$hash = hash('ripemd160', $_POST['password']);
 		if ($hash == $PW){
 			file_put_contents('sponsors.txt', $_POST['sss']);
-			//file_put_contents('endorsements.txt', $_POST['eee']);
+			file_put_contents('e.txt', $_POST['eee']);
 			header("Location: edit.php");
 		}	
 	}
@@ -24,7 +24,7 @@
 	<textarea style="width:1000px;height:300px;" name="sss"><?= $sponsors ?></textarea>
 	<br>
 	<H1> ENDORSEMENTS: </H1>
-	<textarea style="width:1000px;height:300px;" name="eee"><?= $endorsements ?></textarea>
+	<textarea style="width:1000px;height:300px;" name="eee"><?= $e ?></textarea>
 	<br>
 	Password: 
 	<input type="password" name="password">
